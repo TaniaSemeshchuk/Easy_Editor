e373cdb58f1cd64b9663e8705a94a05ae5132d20
Tania Semeshchuk <74670974+TaniaSemeshchuk@users.noreply.github.com>
2024-06-21 21:01:50 +0300

    main.py

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ from PyQt5.QtWidgets import (
    QHBoxLayout, QVBoxLayout
 )
 
+import os
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QPixmap
+from PIL import Image
+
 
 app = QApplication([])
 win = QWidget()       
@@ -36,11 +41,67 @@ row_tools.addWidget(btn_sharp)
 row_tools.addWidget(btn_bw)
 col2.addLayout(row_tools)
 
-
 row.addLayout(col1, 20)
 row.addLayout(col2, 80)
 win.setLayout(row)
 
+workdir = ''
+ 
+def filter(files, extensions):
+   result = []
+   for filename in files:
+       for ext in extensions:
+           if filename.endswith(ext):
+               result.append(filename)
+   return result
+ 
+def chooseWorkdir():
+   global workdir
+   workdir = QFileDialog.getExistingDirectory()
+ 
+def showFilenamesList():
+   extensions = ['.jpg','.jpeg', '.png', '.gif', '.bmp']
+   chooseWorkdir()
+   filenames = filter(os.listdir(workdir), extensions)
+ 
+   lw_files.clear()
+   for filename in filenames:
+       lw_files.addItem(filename)
+ 
+btn_dir.clicked.connect(showFilenamesList)
+
+class ImageProcessor():
+   def __init__(self):
+       self.image = None
+       self.dir = None
+       self.filename = None
+       self.save_dir = "Modified/"
+ 
+   def loadImage(self, dir, filename):
+       ''' під час завантаження запам'ятовуємо шлях та ім'я файлу '''
+       self.dir = dir
+       self.filename = filename
+       image_path = os.path.join(dir, filename)
+       self.image = Image.open(image_path)
+ 
+   def showImage(self, path):
+       lb_image.hide()
+       pixmapimage = QPixmap(path)
+       w, h = lb_image.width(), lb_image.height()
+       pixmapimage = pixmapimage.scaled(w, h, Qt.KeepAspectRatio)
+       lb_image.setPixmap(pixmapimage)
+       lb_image.show()
+ 
+workimage = ImageProcessor()
+ 
+def showChosenImage():
+   if lw_files.currentRow() >= 0:
+       filename = lw_files.currentItem().text()
+       workimage.loadImage(workdir, filename)
+       image_path = os.path.join(workimage.dir, workimage.filename)
+       workimage.showImage(image_path)
+ 
+lw_files.currentRowChanged.connect(showChosenImage)
 
 win.show()
 app.exec()
